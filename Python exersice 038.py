24fc64c7c7210778f4fde1b8b6c1a4c230e7a454
chris <jiaxuanwang2023@gmail.com>
2024-04-30 21:06:22 +0400

    .

--- Python exersice 038.py
+++ Python exersice 038.py
@@ -1,3 +1,4 @@
 m = (input("Enter the month"))
 month = {"January": 31, "February": "28 or 29", "March": 31, "April": 30, "May": 31, "June": 30, "July": 31, "August": 31, "September": 30, "October": 31, "November": 30, "December": 31}
-
+if m in month:
+    print(month.get(m))
