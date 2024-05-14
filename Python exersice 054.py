6c98653db4844b5ca9646dd627c2432a0cca4648
chris <jiaxuanwang2023@gmail.com>
2024-05-14 21:19:55 +0400

    .

---
+++ Python exersice 054.py
@@ -0,0 +1,14 @@
+wavelength = int(input("Enter wavelength"))
+
+if wavelength >= 380 and wavelength <= 450:
+    print("Violet")
+if wavelength >= 450 and wavelength <= 495:
+    print("Blue")
+if wavelength >= 495 and wavelength <= 570:
+    print("Green")
+if wavelength >= 570 and wavelength <= 590:
+    print("Yellow")
+if wavelength >= 590 and wavelength <= 620:
+    print("Orange")
+if wavelength >= 620 and wavelength <= 750:
+    print("Red")
