6ea9dd02940220323d1bc6721d76fc6715615c5d
chris <jiaxuanwang2023@gmail.com>
2024-06-18 17:59:21 +0400

    .

---
+++ Python exersice 099.py
@@ -0,0 +1,13 @@
+def to_base_10(number, base):
+
+
+def from_base_10(number, base):
+
+
+def main():
+        input_base = int(input("Enter the input base (2-16): "))
+        output_base = int(input("Enter the output base (2-16): "))
+        number = input("Enter the number: ")\
+
+
+main()
