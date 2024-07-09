1a5da7d73f7a40afb9e8916186212573d81182ce
chris <jiaxuanwang2023@gmail.com>
2024-07-09 21:17:21 +0400

    .

--- Python exersice 115.py
+++ Python exersice 115.py
@@ -1,3 +1,24 @@
+def translate(word):
+    vowels = "aeiou"
+    if word[0] in vowels:
+        return word + "way"
+    else:
+        for i in range(len(word)):
+            if word[i] in vowels:
+                return word[i:] + word[:i] + "ay"
+        return word + "ay"  
+
+def main():
+    text = input("Enter a line of text: ")
+    words = text.split()
+    
+    pig_latin_words = [translate(word) for word in words]
+    
+    pig_latin_text = ' '.join(pig_latin_words)
+    print("Pig Latin translation:", pig_latin_text)
+
+main()
+
 
 
 
